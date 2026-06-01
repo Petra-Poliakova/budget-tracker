24e2daed74c8585d4fc62de0c323c19ea42df12f
Petra Poliakova <petra.poliakova92@gmail.com>
2026-06-01 18:02:04 +0200

    Add SumaryCard to Reports

--- src/pages/Reports.tsx
+++ src/pages/Reports.tsx
@@ -1,7 +1,20 @@
 //import React from 'react'
+import { Container, Grid } from "@mui/material";
+import {SummaryCard} from "@/components/SummaryCard.tsx";
+
+import {useBudgetOverview} from "@/hooks/useBudgetOverview.tsx";
 
 export const Reports = () => {
+  const {kpiData} = useBudgetOverview();
   return (
-    <div>Reports</div>
+      <Container maxWidth="xl" sx={{mb:2}}>
+        <Grid container spacing={3} sx={{mt: 3, alignItems: 'stretch' }} >
+          {kpiData.map((i) => (
+              <Grid key={i.title} size={{xs: 12, md: 6, lg: 3}} sx={{ display: 'flex', minWidth: 0 }} >
+                <SummaryCard title={i.title} value={i.value} description={i.description} icon={i.icon}/>
+              </Grid>
+          ))}
+        </Grid>
+      </Container>
   )
 }
